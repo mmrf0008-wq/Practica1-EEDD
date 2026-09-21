11f27f7dd62d9ac2cbc27e5760863350490c6380
albema <amm00530@red.ujaen.es>
2026-09-21 15:46:08 +0200

    Avanzada la clase especie

--- Especie.cpp
+++ Especie.cpp
@@ -5,4 +5,34 @@
 #include "Especie.h"
 
 
+string Especie::codigo_especie() const {
+    return codigoEspecie;
+}
 
+void Especie::set_codigo_especie(const string &codigo_especie) {
+    codigoEspecie = codigo_especie;
+}
+
+string Especie::nombre_comun() const {
+    return nombreComun;
+}
+
+void Especie::set_nombre_comun(const string &nombre_comun) {
+    nombreComun = nombre_comun;
+}
+
+string Especie::nombre_cientifico() const {
+    return nombreCientifico;
+}
+
+void Especie::set_nombre_cientifico(const string &nombre_cientifico) {
+    nombreCientifico = nombre_cientifico;
+}
+
+string Especie::tipo_planta() const {
+    return tipoPlanta;
+}
+
+void Especie::set_tipo_planta(const string &tipo_planta) {
+    tipoPlanta = tipo_planta;
+}
--- Especie.h
+++ Especie.h
@@ -15,8 +15,21 @@ private:
 	string nombreCientifico;
 	string tipoPlanta;
 
+public:
+	string codigo_especie() const;
+	void set_codigo_especie(const string &codigo_especie);
 
+	string nombre_comun() const;
+	void set_nombre_comun(const string &nombre_comun);
 
+	string nombre_cientifico() const;
+	void set_nombre_cientifico(const string &nombre_cientifico);
+
+	string tipo_planta() const;
+	void set_tipo_planta(const string &tipo_planta);
+
+	bool operator==(const Especie &arr);
+	bool operator>(const Especie &arr);
 };
 
 
